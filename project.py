8f7eb5f91176e69715c9c17d8f82e48b21c3a1d7
MostafaOthman12 <43732802+MostafaOthman12@users.noreply.github.com>
2018-11-25 22:01:28 +0200

    Update project.py

--- project.py
+++ project.py
@@ -17,7 +17,7 @@ from sqlalchemy.orm import sessionmaker
 app = Flask(__name__)
 
 CLIENT_ID = json.loads(
-    open('client_secret.json', 'r').read())['web']['client_id']
+    open('/var/www/flaskapp/CatalogItem/client_secret.json', 'r').read())['web']['client_id']
 
 # Connect to Database and create database session
 engine = create_engine('sqlite:///restaurantmenuwithusers.db',connect_args={'check_same_thread': False})
