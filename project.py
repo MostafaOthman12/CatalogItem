4d46a0a51b5279890e96a90abfd36634ddc03c7f
MostafaOthman12 <43732802+MostafaOthman12@users.noreply.github.com>
2018-11-26 20:16:51 +0200

    Update project.py

--- project.py
+++ project.py
@@ -48,7 +48,7 @@ def gconnect():
 
     try:
         # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secret.json', scope='')
+        oauth_flow = flow_from_clientsecrets('/var/www/CatalogItem/client_secret.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
